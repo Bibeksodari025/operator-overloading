7b74266ada3f65e7c1f45662a5457dfaabacc600
Bibek Sodari <bibeksodari101@gmail.com>
2025-01-31 12:08:50 +0545

    new commit

--- operator overloading cpp/oop3.cpp
+++ operator overloading cpp/oop3.cpp
@@ -9,7 +9,7 @@ class matrix
     public:
     void getdata()
     {
-        cout<<"enter the matrix:";
+        cout<<"enter the father name:";
         for(int i=0;i<3;i++)
         {
             for(int j=0;j<3;j++)
